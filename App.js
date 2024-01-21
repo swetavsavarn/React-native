ff6fbfbdf9ef18d63fef1228d6fcd35bc091f310
swetavsavarn@gmail.com <swetavsavarn@gmail.com>
2024-01-21 20:03:57 +0530

    Views and text

---
+++ App.js
@@ -0,0 +1,22 @@
+import { Text, View, Button } from "react-native";
+
+function App() {
+//   const handlePress = () => {
+//     Alert.alert("Alert Title", "laura lahsun", [
+//       { text: "OK", onPress: () => console.log("OK Pressed") }
+//     ]);
+//   };
+
+  return (
+    <View>
+      <Text style={{ fontSize: 30 }}>hELLO</Text>
+      <Text style={{ fontSize: 30 }}>lahsun</Text>
+      <Button title="daba"></Button>
+      <Button title="na"></Button>
+      <Button title="daba bhi de" onPress={()=>alert("laurA DE LAHSUN")}></Button>
+      {console.log("waah")}
+    </View>
+  );
+}
+
+export default App;
